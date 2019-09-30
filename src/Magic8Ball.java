c3588288e2f18d308bbbd64dd5cfd38d326e8cca
Camron Caldwell <camron.caldwell.2022@student.lhschools.org>
2019-09-30 09:38:14 -0400

    Added public static void with int and string

---
+++ src/Magic8Ball.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Magic8Ball {
+
+    public static void main(String[]args) {
+        Scanner scanMan;
+        int value;
+        int testing;
+        String question;
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
